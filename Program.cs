2ca43cb61413c3f357bb87b5f80a61ce0ef367e3
Kevin <koo9@hotmail.com>
2020-10-14 11:03:25 -0600

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using ESCPOS_NET;
+using ESCPOS_NET.Emitters;
+using ESCPOS_NET.Utilities;
+
+namespace XprinterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter printer IP address: (e.g. 192.168.0.2) default port is 9000");
+            var ip = Console.ReadLine();
+
+            var printer = new NetworkPrinter(ip, 9000, true);
+
+            var e = new EPSON();
+            printer.Write(ByteSplicer.Combine(
+                e.Initialize(),
+                e.PrintLine("Hello Xprinter!")
+                ));
+
+            Console.Read();
+        }
+    }
+}
